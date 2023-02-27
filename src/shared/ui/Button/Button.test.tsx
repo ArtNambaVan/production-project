450175921312ed28565f2ba2c9c9db43d5276ec1
Artem Klochkov <aklochkov@atypicaldigital.com>
2023-02-27 21:35:33 +0300

    fix tests

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react'
-import { Button } from 'shared/ui/Button/Button'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
 
 describe('Button', () => {
   test('Test clear theme', () => {
-    render(<Button>TEST</Button>)
+    render(<Button theme={ThemeButton.CLEAR}>TEST</Button>)
     expect(screen.getByText('TEST')).toHaveClass('clear')
-    screen.debug()
   })
 })
