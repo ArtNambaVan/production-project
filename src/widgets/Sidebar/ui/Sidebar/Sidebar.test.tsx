cc52808196d44613896802dc1db54546080b895b
Artem Klochkov <aklochkov@atypicaldigital.com>
2023-03-01 18:48:38 +0300

    add visual tool, fix screens

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react'
 import { Sidebar } from './Sidebar'
-import { renderWithTranslation } from 'shared/lib/test/renderWithTranslation/renderWithTranslation'
+
+import { ComponentRender } from 'shared/config/tests/ComponentRender/ComponentRender'
 
 describe('Sidebar', () => {
   test('Sidebar test 1', () => {
-    renderWithTranslation(<Sidebar />)
+    ComponentRender(<Sidebar />, { route: '/' })
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('toggle sidebar', () => {
-    renderWithTranslation(<Sidebar />)
+    ComponentRender(<Sidebar />, { route: '/' })
     const toggleButton = screen.getByTestId('sidebar-toggle')
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     fireEvent.click(toggleButton)
